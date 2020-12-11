1fa221aa2eb47e28441a96ac4385650e2b0e25c0
gameme10 <66317738+gameme10@users.noreply.github.com>
2020-12-11 22:16:55 +0100

    login v2

--- static/js/firebase/auth.js
+++ static/js/firebase/auth.js
@@ -1,68 +1,72 @@
-function ParseURLParametrer(Parametrer){
+var wait = function (tiempo) {
+  return {
+    then: function (f) {
+      setTimeout(f, tiempo);
+    },
+  };
+};
+
+function ParseURLParametrer(Parametrer) {
   var FullURL = window.location.search.substring(1);
   var ParametresArray = FullURL.split("&");
   for (var i = 0; i < ParametresArray.length; i++) {
-    var CurrentParametrer = ParametresArray[i].split('=');
-    if (CurrentParametrer[0] == Parametrer){
-      return CurrentParametrer[1]
+    var CurrentParametrer = ParametresArray[i].split("=");
+    if (CurrentParametrer[0] == Parametrer) {
+      return CurrentParametrer[1];
     }
   }
 }
-var onLogin = ParseURLParametrer('onLogin')
+var onLogin = ParseURLParametrer("onLogin");
 var email = document.getElementById("inputCorreo");
 var password = document.getElementById("inputPassword");
 
 var uid, displayName, photoURL, email2;
 
 function registro() {
-  firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email.value, password.value)
+    .then((user) => {})
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+}
+
+function iniciosesion() {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email.value, password.value)
     .then((user) => {
+      localStorage.setItem("userEmail", user.email);
+      alert(localStorage.getItem("userEmail"));
+      whenislogged();
     })
     .catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
-      console.log(errorCode)
-      console.log(errorMessage)
+      console.log(errorCode);
+      console.log(errorMessage);
     });
-
 }
 
-function iniciosesion() {
-  firebase.auth().signInWithEmailAndPassword(email.value, password.value)
-  .then((user) => {
-    if (typeof onLogin == "undefined"){
-    window.location.replace('../index.html');
-  } else {
-    window.location.replace("../" + onLogin)
-  }
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode)
-    console.log(errorMessage)
-  });
-}
+function whenislogged() {
+  var user = firebase.auth().currentUser;
+  var email, uid;
 
-
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
+  if (user != null) {
+    email = user.email;
     uid = user.uid;
-    displayName = user.displayName
-    photoURL = user.photoURL
-    email2 = user.email
-    var email2local = user.email
-
-        // Crear una clave-valor
-    localStorage.setItem('userEmail', email2local);
-
-
-    // ...
+    if (typeof onLogin !== "undefined") {
+      window.location.replace("../" + onLogin);
+    } else {
+      window.location.replace("../index.html");
+    }
+    // Crear una clave-valor
+    localStorage.setItem("userEmail", uid + "&" + email);
   } else {
-    // Eliminar una clave
-    localStorage.removeItem('userEmail');
+    localStorage.removeItem("userEmail");
   }
-});
-
+}
--- static/js/firebase/data.js
+++ static/js/firebase/data.js
@@ -1,10 +1,18 @@
-var db = firebase.firestore();
-var docRef = db.collection("user_license").doc(email3);
-// Ontener el valor de una clave
 let email3 = localStorage.getItem('userEmail');
-if (email3 = null) {
+
+if (email3 = "undefined") {
     window.location.replace("login/index.html?onLogin=licenses.html"); 
 }
+
+var db = firebase.firestore();
+var docRef = db.collection("user_license").doc(email3array[1]);
+// Ontener el valor de una clave
+var email3array = email3.split("&")
+console.log(email3array);
+console.log("0-- " + email3array[0])
+console.log("1-- " + email3array[1])
+
+
 
 
 // Valid options for source are 'server', 'cache', or
@@ -22,4 +30,7 @@ docRef.get(getOptions).then(function(doc) {
 }).catch(function(error) {
     console.log("Error getting cached document:", error);
 });
+
+
+
 
